dd22a04cbde4221bb55b1feaae00ae72416bdbf8
jinhong <66407386+hongcoding94@users.noreply.github.com>
2022-06-16 13:46:01 +0900

    Create 고양이.java
    Title : 10171 / 고양이
    Date : Todday (16 day June 2022 year)
    Level : 1 (Very Easy)
    Success or False :  ✔️ (Success)

---
+++ BackJoon/입출력과 사칙연산/고양이.java
@@ -0,0 +1,14 @@
+/**
+ * 문제 번호 : 10171번
+ * 문제 제목 : 고양이
+ * 문제 내용 : 아래 예제와 같이 고양이를 출력하시오.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
