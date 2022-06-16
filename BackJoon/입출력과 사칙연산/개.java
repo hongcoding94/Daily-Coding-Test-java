89b99046062dabc9bb8a2a467ab822a7de5ea0e3
jinhong <66407386+hongcoding94@users.noreply.github.com>
2022-06-16 13:47:05 +0900

    Create 개.java
    Title : 10172 / 개
    Date : Todday (16 day June 2022 year)
    Level : 1 (Very Easy)
    Success or False :  ✔️ (Success)

---
+++ BackJoon/입출력과 사칙연산/개.java
@@ -0,0 +1,15 @@
+/**
+ * 문제 번호 : 10172번
+ * 문제 제목 : 개
+ * 문제 내용 : 아래 예제와 같이 개를 출력를 출력하시오.
+ */
+
+public class Main{
+    public static void main(String[]args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");    
+    }
+}
