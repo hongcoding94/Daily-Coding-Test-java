5ef22cf595a69ffa6c0817f4ee4a07f7056d12e1
jinhong <66407386+hongcoding94@users.noreply.github.com>
2022-06-16 14:02:30 +0900

    Create 006. A-B.java
    Title : 1001 / A-B
    Date : Todday (16 day June 2022 year)
    Level : 1 (Very Easy)
    Success or False :  ✔️ (Success)

---
+++ BackJoon/입출력과 사칙연산/006. A-B.java
@@ -0,0 +1,26 @@
+/**
+ * 문제 번호 : 1001번
+ * 문제 제목 : A-B
+ * 문제 내용 : 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+ */
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+      // 스캐너 생성
+	    Scanner in = new Scanner(System.in);
+      
+      // 변수 생성 및 데이터 첨가
+	    int i = in.nextInt();
+	    int j = in.nextInt();
+
+      // 로직
+	    if(0 < i && j < 10){
+	    	int k = i - j;
+	        System.out.println(k);
+	    	}
+      
+      in.close();
+	}
+}
