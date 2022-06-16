d9b5142f1cde5f5bcf23d7e4c1725c91c87a0abd
jinhong <66407386+hongcoding94@users.noreply.github.com>
2022-06-16 13:39:22 +0900

    Daily Coding Test Commit!
    Title : 2557 / Hello World
    Date : Todday (16 day June 2022 year)
    Level : 1 (Very Easy)
    Success or False :  ✔️ (Success)

---
+++ BackJoon/입출력과 사칙연산/Hello World.java
@@ -0,0 +1,11 @@
+/**
+ * 문제 번호 : 2557번
+ * 문제 제목 : Hello World
+ * 문제 내용 : Hello World!를 출력하시오.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
