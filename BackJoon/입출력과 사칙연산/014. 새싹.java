873c90cac2e54cd3176af984563b36d66aca6246
jinhong <66407386+hongcoding94@users.noreply.github.com>
2022-06-16 16:04:35 +0900

    Create 014. 새싹.java
    Title : 25083 / 새싹
    Date : Todday (16 day June 2022 year)
    Level : 1 (Very Easy)
    Success or False :  ✔️ (Success)

---
+++ BackJoon/입출력과 사칙연산/014. 새싹.java
@@ -0,0 +1,18 @@
+/**
+ * 문제 번호 : 25083번
+ * 문제 제목 : 새싹
+ * 문제 내용 : 아래 예제와 같이 새싹을 출력하시오.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        //String
+		String img = "         ,r'\"7\n" + 	
+				   "r`-_   ,'  ,/\n" + 		
+				   " \\. \". L_r'\n" + 		
+				   "   `~\\/\n" + 			
+				   "      |\n" + 			
+				   "      |";
+        System.out.println(img);
+    }
+}
