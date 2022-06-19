c80fb5232f1f16ac91e1beb2a045397b0b312260
jinhong <66407386+hongcoding94@users.noreply.github.com>
2022-06-19 12:53:14 +0900

    Create 007. A+B - 7.java
    Title : 11021 / A+B - 7
    Date : Today (19 day June 2022 year)
    Level : 1 (Very Easy)
    Success or False :  ✔️ (Success)

---
+++ BackJoon/003. 반복문/007. A+B - 7.java
@@ -0,0 +1,29 @@
+
+/**
+ * 문제 번호 : 11021번
+ * 문제 제목 : A+B - 7
+ * 문제 내용 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ */
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// 스캐너 생성
+		Scanner in = new Scanner(System.in);		
+
+		// 입력 받을 전역 변수
+		int i = in.nextInt(); 
+
+		// 로직
+		for(int j=1; j<=i; j++) {
+			int h = in.nextInt();
+			int k = in.nextInt();
+			System.out.println("Case #"+ j + ": " + (h+k));
+		}
+
+		// 자원 닫기
+		in.close();
+
+	}
+}
