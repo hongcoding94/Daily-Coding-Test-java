2d0a3bd8670f97f20b56bffa927d7ddb7dd84399
jinhong <66407386+hongcoding94@users.noreply.github.com>
2022-06-29 02:35:46 +0900

    Create 001. 그대로 출력하기.java
    Title : 11718 / 그대로 출력하기
    Date : Today (29th June 2022 year)
    Level : 1 (Very Easy)
    Success or False :  ✔️ (Success)

---
+++ BackJoon/000. 분류/문자열/001. 그대로 출력하기.java
@@ -0,0 +1,19 @@
+/**
+ * 문제 번호 : 11718번
+ * 문제 제목 : 그대로 출력하기
+ * 문제 내용 : 입력 받은 대로 출력하는 프로그램을 작성하시오.
+ */
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        while(in.hasNext()){
+            System.out.println(in.nextLine());
+        }
+        
+        in.close();
+    }
+}
