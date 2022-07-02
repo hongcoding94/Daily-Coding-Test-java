822d12989a204c3fd9f026603b4d6059c2afa172
jinhong <66407386+hongcoding94@users.noreply.github.com>
2022-07-02 16:58:41 +0900

    Update 009. 숫자만 추출.java
    Create 009. 숫자만 추출.java
    Title :   009. 숫자만 추출
    Date : Today (2th July 2022 year)
    Contents : Watch the video, these's two method

--- 인프런/자바 알고리즘 문제풀이/009. 숫자만 추출.java
+++ 인프런/자바 알고리즘 문제풀이/009. 숫자만 추출.java
@@ -76,7 +76,7 @@ public class Main {
     public static void main(String[] args){
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        
+         
         String str = kb.next();
         System.out.println(T.solution(str));
     }
