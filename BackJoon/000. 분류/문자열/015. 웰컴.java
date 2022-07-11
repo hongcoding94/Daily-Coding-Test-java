2bca73f83f573c8da24d6d5c4ac755548b807cbb
jinhong <66407386+hongcoding94@users.noreply.github.com>
2022-07-11 09:53:31 +0900

    Create 015. 웰컴.java
    Title :  5337 / 웰컴
    Date : Today (１1th July 2022 year)
    Level : 브론즈Ⅴ
    Success or False :  ✔️ (Success)

---
+++ BackJoon/000. 분류/문자열/015. 웰컴.java
@@ -0,0 +1,12 @@
+/**
+ * 문제 번호 : 5337번
+ * 문제 제목 : 웰컴
+ * 문제 내용 : 꼬마 정민이는 이제 A + B 정도는 쉽게 계산할 수 있다. 이제 A + B + C를 계산할 차례이다!
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(".  .   .\n" + "|  | _ | _. _ ._ _  _\n" + "|/\\|(/.|(_.(_)[ | )(/.\n");
+    }
+}
+
